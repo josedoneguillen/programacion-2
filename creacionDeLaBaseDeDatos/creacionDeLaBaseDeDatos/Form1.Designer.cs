5f1eaada1f029ca3cb78d81e058d9d04aad92002
josedoneguillen <josedoneguillen1992@hotmail.com>
2021-04-22 16:58:15 -0400

    New project working with database conection with mysql for unity 3 - activity 2

---
+++ creacionDeLaBaseDeDatos/creacionDeLaBaseDeDatos/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace creacionDeLaBaseDeDatos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.ID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Usuario = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Clave = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Nombre = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Apellido = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Correo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Estatus = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Fecha_Creado = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Foto = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Imagen = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ID,
+            this.Usuario,
+            this.Clave,
+            this.Nombre,
+            this.Apellido,
+            this.Correo,
+            this.Estatus,
+            this.Fecha_Creado,
+            this.Foto,
+            this.Imagen});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(800, 450);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // ID
+            // 
+            this.ID.Text = "ID";
+            // 
+            // Usuario
+            // 
+            this.Usuario.Text = "Usuario";
+            // 
+            // Clave
+            // 
+            this.Clave.Text = "Clave";
+            // 
+            // Nombre
+            // 
+            this.Nombre.Text = "Nombre";
+            // 
+            // Apellido
+            // 
+            this.Apellido.Text = "Apellido";
+            // 
+            // Correo
+            // 
+            this.Correo.Text = "Correo";
+            // 
+            // Estatus
+            // 
+            this.Estatus.Text = "Estatus";
+            // 
+            // Fecha_Creado
+            // 
+            this.Fecha_Creado.Text = "Fecha_Creado";
+            this.Fecha_Creado.Width = 120;
+            // 
+            // Foto
+            // 
+            this.Foto.Text = "Foto";
+            // 
+            // Imagen
+            // 
+            this.Imagen.Text = "Imagen";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader ID;
+        private System.Windows.Forms.ColumnHeader Usuario;
+        private System.Windows.Forms.ColumnHeader Clave;
+        private System.Windows.Forms.ColumnHeader Nombre;
+        private System.Windows.Forms.ColumnHeader Apellido;
+        private System.Windows.Forms.ColumnHeader Correo;
+        private System.Windows.Forms.ColumnHeader Estatus;
+        private System.Windows.Forms.ColumnHeader Fecha_Creado;
+        private System.Windows.Forms.ColumnHeader Foto;
+        private System.Windows.Forms.ColumnHeader Imagen;
+    }
+}
+
---
+++ creacionDeLaBaseDeDatos/creacionDeLaBaseDeDatos/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace creacionDeLaBaseDeDatos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            {
+                MySqlConnection con = new MySqlConnection("server=localhost;user id=root;password=Abcd1234.;database=programacion2;sslMode=none");
+                MySqlCommand cmd;
+                MySqlDataAdapter da;
+                DataTable dt;
+
+
+                try
+                {
+                    con.Open();
+                    cmd = new MySqlCommand();
+                    cmd.Connection = con;
+                    cmd.CommandText = "Select * FROM usuario";
+                    da = new MySqlDataAdapter();
+                    da.SelectCommand = cmd;
+                    dt = new DataTable();
+                    da.Fill(dt);
+
+                    for (int i = 0; i < dt.Rows.Count; i++)
+                    {
+                        DataRow dr = dt.Rows[i];
+                        ListViewItem listitem = new ListViewItem(dr["ID"].ToString());
+                        listitem.SubItems.Add(dr["Usuario"].ToString());
+                        listitem.SubItems.Add(dr["Clave"].ToString());
+                        listitem.SubItems.Add(dr["Nombre"].ToString());
+                        listitem.SubItems.Add(dr["Apellido"].ToString());
+                        listitem.SubItems.Add(dr["Correo"].ToString());
+                        listitem.SubItems.Add(dr["Estatus"].ToString());
+                        listitem.SubItems.Add(dr["Fecha_Creado"].ToString());
+                        listitem.SubItems.Add(dr["Foto"].ToString());
+                        listitem.SubItems.Add(dr["Imagen"].ToString());
+                        listView1.Items.Add(listitem);
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+        }
+    }
+}
