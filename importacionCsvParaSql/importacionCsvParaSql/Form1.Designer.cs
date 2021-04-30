9e29b93bb200e632a1b761dad7b0b48c3cc4c9aa
Jose Doñe <josedoneguillen1992@hotmail.com>
2021-04-30 17:16:06 -0400

    New project read and use csv file data to insert into a database for unity 5 activity 2

---
+++ importacionCsvParaSql/importacionCsvParaSql/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace importacionCsvParaSql
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.ARTICULO = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.CANTIDAD = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.PRECIO = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.ID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 75F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Controls.Add(this.listView1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(640, 360);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ID,
+            this.ARTICULO,
+            this.CANTIDAD,
+            this.PRECIO});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(3, 3);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(474, 354);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // ARTICULO
+            // 
+            this.ARTICULO.Text = "ARTICULO";
+            this.ARTICULO.Width = 240;
+            // 
+            // CANTIDAD
+            // 
+            this.CANTIDAD.Text = "CANTIDAD";
+            this.CANTIDAD.Width = 120;
+            // 
+            // PRECIO
+            // 
+            this.PRECIO.Text = "PRECIO";
+            this.PRECIO.Width = 120;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(483, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Cargar Informacion";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ID
+            // 
+            this.ID.Text = "ID";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader ARTICULO;
+        private System.Windows.Forms.ColumnHeader CANTIDAD;
+        private System.Windows.Forms.ColumnHeader PRECIO;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColumnHeader ID;
+    }
+}
+
---
+++ importacionCsvParaSql/importacionCsvParaSql/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using MySql.Data.MySqlClient;
+
+namespace importacionCsvParaSql
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void loadData()
+        {
+            MySqlConnection con = new MySqlConnection("server=localhost;user id=root;password=Abcd1234.;database=articulos;sslMode=none");
+            MySqlCommand cmd;
+            MySqlDataAdapter da;
+            DataTable dt;
+
+
+            try
+            {
+                con.Open();
+                cmd = new MySqlCommand();
+                cmd.Connection = con;
+                cmd.CommandText = "Select * FROM articulos";
+                da = new MySqlDataAdapter();
+                da.SelectCommand = cmd;
+                dt = new DataTable();
+                da.Fill(dt);
+
+                listView1.Items.Clear();
+
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    DataRow dr = dt.Rows[i];
+                    ListViewItem listitem = new ListViewItem(dr["ID"].ToString());
+                    listitem.SubItems.Add(dr["ARTICULO"].ToString());
+                    listitem.SubItems.Add(dr["CANTIDAD"].ToString());
+                    listitem.SubItems.Add(dr["PRECIO"].ToString());
+                    listView1.Items.Add(listitem);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string archivo = File.ReadAllText(@"C:\Users\JDONE_LAPTOP\source\repos\UNICARIBE\programacion-2\importacionCsvParaSql\datos.csv");
+            string query = "INSERT INTO `articulos` (`ARTICULO`, `CANTIDAD`, `PRECIO`) VALUES ";
+            int n_fila = 0;
+            int t_filas = archivo.Split('\n').Length;
+            int t_registro = 0;
+            int n_registro = 0;
+
+            foreach (string fila in archivo.Split('\n'))
+            {
+                if (!string.IsNullOrEmpty(fila)) 
+                {
+                   
+                    t_registro = fila.Split(',').Length;
+                    n_registro = 0;
+
+                    query += "( ";
+
+                    foreach (string registro in fila.Split(','))
+                    {
+                        query += "'" + registro + "'";
+
+                        if (n_registro < (t_registro - 1))
+                        {
+                            query += ", ";
+                        }
+
+                        n_registro++;
+
+                    }
+
+                    query += ") ";
+
+                    if (n_fila < (t_filas - 1) ) 
+                    {
+                        query += ", ";
+                    }
+
+                    n_fila++;
+                }
+            }
+
+            MySqlConnection MyConn = new MySqlConnection("server=localhost;user id=root;password=Abcd1234.;database=articulos;sslMode=none");
+
+
+            try
+            {
+
+                MySqlCommand MyCommand2 = new MySqlCommand(query, MyConn);
+                MySqlDataReader MyReader2;
+
+                MyConn.Open();
+                MyReader2 = MyCommand2.ExecuteReader();
+
+                while (MyReader2.Read())
+                {
+                }
+
+                MessageBox.Show("Informacion Agregada");
+
+                loadData();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                MyConn.Close();
+            }
+
+        }
+    }
+}
